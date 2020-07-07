70d58883ece24ea502c31f545ee93dc4d1219285
Lalit Ponnala <lalit.p@gmail.com>
2020-07-07 03:33:21 +0000

    commit

--- 2020-07-06/script.R
+++ 2020-07-06/script.R
@@ -4,7 +4,7 @@
 # see 2014-11-24/analyz.R
 
 clust_datafile = "ABC1K_proteomicsDB_expression-clustering-forLalit.xlsx"
-
+D = readxl::read_excel(clust_datafile)
 
 # ----------------------------------------
 # // Differential Expression //

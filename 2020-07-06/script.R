e2dc99cd29991bc82f5ebe2d593f9b709fbf42e6
Lalit Ponnala <lalit.p@gmail.com>
2020-07-07 03:29:05 +0000

    commit

---
+++ 2020-07-06/script.R
@@ -0,0 +1,14 @@
+
+# ----------------------------------------
+# // Heirarchical Clustering //
+# see 2014-11-24/analyz.R
+
+clust_datafile = "ABC1K_proteomicsDB_expression-clustering-forLalit.xlsx"
+
+
+# ----------------------------------------
+# // Differential Expression //
+
+diffexp_datafile = "pgs-elena-forlalit-Glee_annovatest.xlsx"
+
+# ----------------------------------------

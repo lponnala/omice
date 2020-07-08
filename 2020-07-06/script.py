4bc95917dac7b88cb055e5e3ece279bb9b8897e6
Lalit Ponnala <lalit.ponnala@spglobal.com>
2020-07-08 01:01:02 -0400

    commit

--- 2020-07-06/script.py
+++ 2020-07-06/script.py
@@ -16,9 +16,47 @@ D = pd.read_excel(clust_datafile)
 
 D = D.iloc[:,:4]
 maxValue = D.groupby('Accession')['Average Normalized Intensity'].max().reset_index().rename({'Average Normalized Intensity':'Max Intensity'},axis=1)
-Y = pd.merge(D, maxValue, how='left', on='Accession').assign(Intensity = lambda X: X['Average Normalized Intensity']/X['Max Intensity'])
-Y = Y.pivot(index='Accession', columns='Tissue', values='Intensity').fillna(0)
-Y.reset_index().to_csv("cluster_data.csv",index=False)
+X = pd.merge(D, maxValue, how='left', on='Accession').assign(Intensity = lambda X: X['Average Normalized Intensity']/X['Max Intensity'])
+X = X.pivot(index='Accession', columns='Tissue', values='Intensity').fillna(0)
+X.reset_index().to_csv("cluster_data.csv",index=False)
+
+from scipy.cluster.hierarchy import dendrogram, linkage
+from matplotlib import pyplot as plt
+
+linked = linkage(X, method='average', metric='correlation')
+labelList = X.index.values
+plt.figure(figsize=(10, 7))
+dendrogram(linked,
+            orientation='top',
+            labels=labelList,
+            distance_sort='descending',
+            show_leaf_counts=True)
+plt.show()
+
+# import numpy as np
+# from scipy.cluster.hierarchy import dendrogram, linkage
+# from matplotlib import pyplot as plt
+# 
+# X = np.array([[5,3],
+#     [10,15],
+#     [15,12],
+#     [24,10],
+#     [30,30],
+#     [85,70],
+#     [71,80],
+#     [60,78],
+#     [70,55],
+#     [80,91],])
+# linked = linkage(X, 'single')
+# labelList = range(1, 11)
+# plt.figure(figsize=(10, 7))
+# dendrogram(linked,
+#             orientation='top',
+#             labels=labelList,
+#             distance_sort='descending',
+#             show_leaf_counts=True)
+# plt.show()
+
 
 # # ----------------------------------------
 # # // Differential Expression //

9b8a015ef40f3397241d8e9994f27ec31c1cdfeb
Lalit Ponnala <lalit.p@gmail.com>
2020-07-10 01:57:29 +0000

    commit

--- 2020-07-06/Rscript.R
+++ 2020-07-06/Rscript.R
@@ -4,21 +4,59 @@
 # see script.py for preparing the data
 # [the following analysis is based on 2014-11-24/analyz.R]
 
-DATA = readr::read_csv("cluster_data_byProtein.csv")
+library(readr)
+library(gplots)
+
+# // By Protein //
+data_file = "cluster_data_byProtein.csv"
+dendro_file = "clusters_byProtein.png"
+heatmap_file = "heatmap_byProtein.png"
+
+DATA = readr::read_csv(data_file)
 dim(DATA)
 colnames(DATA)
-
 D = DATA[,-1]
 sapply(D,class)
 unique(sapply(D,class))
 
 # ~~ Dendrogram ~~
 save_fig = TRUE
-
 D_dd = as.dist((1-cor(t(D)))/2)
 D_hc = hclust(D_dd, method="average")
 if (save_fig) {
-    png(filename = "clusters.png", width=960, height=480, units="px")
+    png(filename = dendro_file, width=960, height=480, units="px")
+}
+plot(D_hc, labels=unlist(DATA[,1],use.names=FALSE), hang=-1, frame.plot=FALSE, main="Heirarchical Clusters", sub="", xlab="", ylab="correlation-based distance")
+num_clust = 4
+rect.hclust(D_hc, k=num_clust, border = 1 + 1:num_clust)
+if (save_fig) {
+    dev.off()
+}
+
+# ~~ Heatmap ~~
+Dm = as.matrix(D)
+png(filename = heatmap_file, width=960, height=480, units="px")
+heatmap.2(Dm, Rowv = as.dendrogram(D_hc), dendrogram = "row", col=redgreen(75), scale="row", key=TRUE, density.info="none", trace="none", cexCol=0.8, labRow=NA)
+dev.off()
+
+# // By Tissue //
+data_file = "cluster_data_byTissue.csv"
+dendro_file = "clusters_byTissue.png"
+heatmap_file = "heatmap_byTissue.png"
+
+DATA = readr::read_csv(data_file)
+dim(DATA)
+colnames(DATA)
+D = DATA[,-1]
+sapply(D,class)
+unique(sapply(D,class))
+
+# ~~ Dendrogram ~~
+save_fig = TRUE
+D_dd = as.dist((1-cor(t(D)))/2)
+D_hc = hclust(D_dd, method="average")
+if (save_fig) {
+    png(filename = dendro_file, width=960, height=480, units="px")
 }
 plot(D_hc, labels=unlist(DATA[,1],use.names=FALSE), hang=-1, frame.plot=FALSE, main="Heirarchical Clusters", sub="", xlab="", ylab="correlation-based distance")
 num_clust = 4
@@ -29,15 +67,8 @@ if (save_fig) {
 
 # ~~ Heatmap ~~
 library(gplots)
-
 Dm = as.matrix(D)
-# colnames(Dm) = c("WT-FL", "Met1-FL", "WT-NL", "Met1-NL")
-
-# helpful links for heatmap
-# https://www.biostars.org/p/14156/
-# http://mannheimiagoesprogramming.blogspot.com/2012/06/drawing-heatmaps-in-r-with-heatmap2.html
-
-png(filename = "heatmap.png", width=960, height=480, units="px")
+png(filename = heatmap_file, width=960, height=480, units="px")
 heatmap.2(Dm, Rowv = as.dendrogram(D_hc), dendrogram = "row", col=redgreen(75), scale="row", key=TRUE, density.info="none", trace="none", cexCol=0.8, labRow=NA)
 dev.off()
 

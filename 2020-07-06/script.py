40f35992beddeb5eed4bb3992ee67869b46a14b4
Lalit Ponnala <lalit.p@gmail.com>
2020-07-10 01:30:54 +0000

    commit

--- 2020-07-06/script.py
+++ 2020-07-06/script.py
@@ -17,38 +17,44 @@ D = pd.read_excel(clust_datafile)
 # len(D['Accession'].unique())
 # len(D['Tissue (27 types)'].dropna().values)
 # D['Tissue'].value_counts()
+D = D.iloc[:,:4]
 
 # per Klaas' email, normalize each protein by the max expression value across 27 tissue types
-D = D.iloc[:,:4]
 maxValue = D.groupby('Accession')['Average Normalized Intensity'].max().reset_index().rename({'Average Normalized Intensity':'Max Intensity'},axis=1)
-X = pd.merge(D, maxValue, how='left', on='Accession').assign(Intensity = lambda X: X['Average Normalized Intensity']/X['Max Intensity'])
-X = X.pivot(index='Accession', columns='Tissue', values='Intensity').fillna(0)
+P = pd.merge(D, maxValue, how='left', on='Accession').assign(Intensity = lambda X: X['Average Normalized Intensity']/X['Max Intensity'])
+P = P.pivot(index='Accession', columns='Tissue', values='Intensity').fillna(0)
+
+# per Klaas' second email, normalize each tissue by the max expression value across all proteins
+maxValue = D.groupby('Tissue')['Average Normalized Intensity'].max().reset_index().rename({'Average Normalized Intensity':'Max Intensity'},axis=1)
+T = pd.merge(D, maxValue, how='left', on='Tissue').assign(Intensity = lambda X: X['Average Normalized Intensity']/X['Max Intensity'])
+T = T.pivot(index='Tissue', columns='Accession', values='Intensity').fillna(0)
 
 # save the normalized-to-max data to be used for clustering
-X.reset_index().to_csv("cluster_data.csv",index=False)
+P.reset_index().to_csv("cluster_data_byProtein.csv",index=False)
+T.reset_index().to_csv("cluster_data_byTissue.csv",index=False)
 
-# draw dendrogram to identify the number of clusters
-# !! can't mark rectanges around the clusters, so go with the R version in Rscript.R !!
-linked = linkage(X, method='average', metric='correlation')
-labelList = X.index.values
-plt.figure(figsize=(10, 7))
-dendrogram(linked,
-            # color_threshold=0.8,
-            truncate_mode='lastp', p = 17,
-            orientation='top',
-            labels=labelList,
-            distance_sort='descending',
-            show_leaf_counts=True, leaf_rotation=90)
-plt.tight_layout()
-plt.show()
+# # draw dendrogram to identify the number of clusters
+# # !! can't mark rectanges around the clusters, so go with the R version in Rscript.R !!
+# linked = linkage(P, method='average', metric='correlation')
+# labelList = P.index.values
+# plt.figure(figsize=(10, 7))
+# dendrogram(linked,
+#             # color_threshold=0.8,
+#             truncate_mode='lastp', p = 17,
+#             orientation='top',
+#             labels=labelList,
+#             distance_sort='descending',
+#             show_leaf_counts=True, leaf_rotation=90)
+# plt.tight_layout()
+# plt.show()
 
-# find the contents of each cluster (matches with the R version in Rscript.R)
-num_clust = 4
-cluster = AgglomerativeClustering(n_clusters=num_clust, affinity='correlation', linkage='average')
-y = cluster.fit_predict(X)
-for k in range(num_clust):
-    print(f"cluster {k+1}")
-    print(X[y == k].index)
+# # find the contents of each cluster (matches with the R version in Rscript.R)
+# num_clust = 4
+# cluster = AgglomerativeClustering(n_clusters=num_clust, affinity='correlation', linkage='average')
+# y = cluster.fit_predict(P)
+# for k in range(num_clust):
+#     print(f"cluster {k+1}")
+#     print(P[y == k].index)
 
 
 # ----------------------------------------

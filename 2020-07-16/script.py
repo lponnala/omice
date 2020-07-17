0cab4c6ea9f354d3185dda9dec3b6653177468ea
Lalit Ponnala <lalit.p@gmail.com>
2020-07-17 13:21:32 +0000

    commit

--- 2020-07-16/script.py
+++ 2020-07-16/script.py
@@ -5,6 +5,12 @@ data_file = "forLalit-3sets-forclusterproteome.xlsx"
 
 X = pd.read_excel(data_file, sheet_name="set1-CLP", skiprows=1)
 
+if X.shape[0] != len(X['Accession'].drop_duplicates()):
+    raise Exception("duplicate accession numbers found!")
+
+X = X.drop(['our interests','group','lab annotation'],axis=1)
+print(X.isna().sum())
+X = X.fillna(0)
 
 
 

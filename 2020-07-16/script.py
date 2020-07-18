3c9c4d4e5f4b057fd4ac9127619491902c5be53d
Lalit Ponnala <lalit.p@gmail.com>
2020-07-18 16:10:09 +0000

    commit

--- 2020-07-16/script.py
+++ 2020-07-16/script.py
@@ -16,6 +16,12 @@ X = X.fillna(0).set_index('Accession').rename_axis(columns='Tissue')
 P = X / X.max(axis=0)
 T = X.T / X.max(axis=1)
 
+if P.isna().any().any():
+    raise Exception("NaN values in P")
+
+if T.isna().any().any():
+    raise Exception("NaN values in T")
+
 P.reset_index().to_csv("set1_data_byProtein.csv",index=False)
 T.reset_index().to_csv("set1_data_byTissue.csv",index=False)
 
@@ -33,6 +39,12 @@ X = X.fillna(0).set_index('Accession').rename_axis(columns='Tissue')
 P = X / X.max(axis=0)
 T = X.T / X.max(axis=1)
 
+if P.isna().any().any():
+    raise Exception("NaN values in P")
+
+if T.isna().any().any():
+    raise Exception("NaN values in T")
+
 P.reset_index().to_csv("set2_data_byProtein.csv",index=False)
 T.reset_index().to_csv("set2_data_byTissue.csv",index=False)
 
@@ -49,6 +61,12 @@ X = X.fillna(0).set_index('Accession').rename_axis(columns='Tissue')
 
 P = X / X.max(axis=0)
 T = X.T / X.max(axis=1)
+
+if P.isna().any().any():
+    raise Exception("NaN values in P")
+
+if T.isna().any().any():
+    raise Exception("NaN values in T")
 
 P.reset_index().to_csv("set3_data_byProtein.csv",index=False)
 T.reset_index().to_csv("set3_data_byTissue.csv",index=False)

0856d773afd775e8a261428150aa01b4b780e2bd
Lalit Ponnala <lalit.p@gmail.com>
2020-09-22 00:33:08 -0400

    commit

--- 2020-09-21/script.py
+++ 2020-09-21/script.py
@@ -2,30 +2,31 @@
 import pandas as pd
 
 A = pd.read_excel("ATHENA-PGs-ABC1Ks-forLalit.xlsx",sheet_name=None,skiprows=1)
-key = ['PGs','17-ABC1Ks'][1]
 
-df = A[key].drop(columns=['our interests','group','lab annotation']).set_index('Accession')
+for key in A.keys():
+    print("\n",key,"\n",sep="")
+    df = A[key].drop(columns=['our interests','group','lab annotation']).set_index('Accession')
 
-# remove proteins that have all zero values (since they create problems for the correlation-based distance matrix)
-if any(df.isna().sum(axis=1) == df.shape[1]):
-    print(f"dropping {sum(df.isna().sum(axis=1) == df.shape[1])} all-missing row(s)")
-    df = df.loc[df.isna().sum(axis=1) < df.shape[1],:]
+    # remove proteins that have all zero values (since they create problems for the correlation-based distance matrix)
+    if any(df.isna().sum(axis=1) == df.shape[1]):
+        print(f"dropping {sum(df.isna().sum(axis=1) == df.shape[1])} all-missing row(s)")
+        df = df.loc[df.isna().sum(axis=1) < df.shape[1],:]
 
-# fill missing values with zero
-df = df.fillna(0)
+    # fill missing values with zero
+    df = df.fillna(0)
 
-# scale each row by its max (i.e. by the max across all tissues)
-df_s = df.apply(lambda x: x/x.max(), axis=1)
-df_s.to_csv(f"{key}_Scaled_data.csv")
+    # scale each row by its max (i.e. by the max across all tissues)
+    df_s = df.apply(lambda x: x/x.max(), axis=1)
+    df_s.to_csv(f"{key}_Scaled_data.csv")
 
-# note: dividing by the max seems better suited (instead of min-max scaling) because:
-# - we don't want to replace the minimum (across tissues) in each protein with zero
-# - it will be roughly the same as min-max scaling since we're using correlation as the distance measure
+    # note: dividing by the max seems better suited (instead of min-max scaling) because:
+    # - we don't want to replace the minimum (across tissues) in each protein with zero
+    # - it will be roughly the same as min-max scaling since we're using correlation as the distance measure
 
-# # min-max scaling of each row
-# df_mm = df.apply(lambda x: (x-x.min())/(x.max()-x.min()), axis=1)
+    # # min-max scaling of each row
+    # df_mm = df.apply(lambda x: (x-x.min())/(x.max()-x.min()), axis=1)
 
-# apply z-score transformation for each row
-df_z = df.apply(lambda x: (x-x.mean())/x.std(ddof=1), axis=1)
-df_z.to_csv(f"{key}_Zscore_data.csv")
+    # apply z-score transformation for each row
+    df_z = df.apply(lambda x: (x-x.mean())/x.std(ddof=1), axis=1)
+    df_z.to_csv(f"{key}_Zscore_data.csv")
 

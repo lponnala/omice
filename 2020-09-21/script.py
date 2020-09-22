177afd278267c85d2e42f51795bfd78185a5ce65
Lalit Ponnala <lalit.p@gmail.com>
2020-09-21 23:46:20 -0400

    commit

--- 2020-09-21/script.py
+++ 2020-09-21/script.py
@@ -1,17 +1,17 @@
 
 import pandas as pd
 
-A = pd.read_excel("F:/download/ATHENA-PGs-ABC1Ks-forLalit.xlsx",sheet_name=None,skiprows=1)
+A = pd.read_excel("ATHENA-PGs-ABC1Ks-forLalit.xlsx",sheet_name=None,skiprows=1)
 
-df = A['PGs'].set_index(['our interests', 'group', 'Accession', 'lab annotation'])
-# df = A['17-ABC1Ks'].set_index(['our interests', 'group', 'Accession', 'lab annotation'])
+df = A['PGs'].drop(columns=['our interests','group','lab annotation']).set_index('Accession')
+# df = A['17-ABC1Ks'].drop(columns=['our interests','group','lab annotation']).set_index('Accession')
 
 if any(df.isna().sum(axis=0) == df.shape[0]):
-    print(f"{sum(df.isna().sum(axis=0) == df.shape[0])} all-missing column(s) found!")
+    print(f"dropping {sum(df.isna().sum(axis=0) == df.shape[0])} all-missing column(s)")
     df = df.loc[:,df.isna().sum(axis=0) < df.shape[0]]
 
 if any(df.isna().sum(axis=1) == df.shape[1]):
-    print(f"{sum(df.isna().sum(axis=1) == df.shape[1])} all-missing row(s) found!")
+    print(f"dropping {sum(df.isna().sum(axis=1) == df.shape[1])} all-missing row(s)")
     df = df.loc[df.isna().sum(axis=1) < df.shape[1],:]
 
 # fill missing values with zero
@@ -19,6 +19,10 @@ df = df.fillna(0)
 
 # standardize each row by its max (i.e. by the max across all tissues)
 df_s = df.apply(lambda x: x/x.max(), axis=1)
+
+# note: dividing by the max seems better suited (instead of min-max scaling) because:
+# - we don't want to replace the minimum (across tissues) in each protein with zero
+# - it will be roughly the same as min-max scaling since we're using correlation as the distance measure
 
 # # min-max scaling of each row
 # df_mm = df.apply(lambda x: (x-x.min())/(x.max()-x.min()), axis=1)

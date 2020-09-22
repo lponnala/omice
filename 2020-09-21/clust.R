8ebf7422f158c3b1c94c6917e95c5d1f64eb7c26
Lalit Ponnala <lalit.p@gmail.com>
2020-09-22 00:08:46 -0400

    commit

---
+++ 2020-09-21/clust.R
@@ -0,0 +1,14 @@
+
+# ----------------------------------------
+# // Heirarchical Clustering //
+# see script.py for preparing the data
+
+library(readr)
+library(gplots)
+
+for (set in c("PGs","ABC1Ks")) {
+    for (typ in c("scaled","zscore")) {
+        data_file = 
+    }
+}
+

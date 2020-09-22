8b596bafccd30f170e08138069efba052d6b729c
Lalit Ponnala <lalit.p@gmail.com>
2020-09-22 00:19:32 -0400

    commit

--- 2020-09-21/clust.R
+++ 2020-09-21/clust.R
@@ -6,11 +6,11 @@
 library(readr)
 library(gplots)
 
-for (set in c("PGs","ABC1Ks")) {
+for (key in c("PGs","17-ABC1Ks")) {
     for (typ in c("scaled","zscore")) {
-        cat(paste0("\n","-- ",set,",",typ," --","\n"))
-        data_file = paste0(set,'_',typ,'_data.csv')
-        dendro_file = paste0(set,'_',typ,'_clusters.png')
+        cat(paste0("\n","-- ",key,",",typ," --","\n"))
+        data_file = paste0(key,'_',typ,'_data.csv')
+        dendro_file = paste0(key,'_',typ,'_clusters.png')
 
         DATA = readr::read_csv(data_file)
         dim(DATA)
@@ -24,7 +24,7 @@ for (set in c("PGs","ABC1Ks")) {
         D_hc = hclust(D_dd, method="average")
         png(filename = dendro_file, width=960, height=480, units="px")
         plot(D_hc, labels=unlist(DATA[,1],use.names=FALSE), hang=-1, frame.plot=FALSE, main="Heirarchical Clusters", sub="", xlab="", ylab="correlation-based distance")
-        # num_clust = c(set1 = 8, set2 = 8, set3 = 5)[set]
+        # num_clust = c(set1 = 8, set2 = 8, set3 = 5)[key]
         # rect.hclust(D_hc, k=num_clust, border = 1 + 1:num_clust)
         dev.off()
     }

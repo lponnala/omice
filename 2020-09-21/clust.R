eecc4597eaceba370bcc49c872f8855c5d3dd0b9
Lalit Ponnala <lalit.p@gmail.com>
2020-09-27 10:40:31 -0400

    commit

--- 2020-09-21/clust.R
+++ 2020-09-21/clust.R
@@ -6,31 +6,31 @@
 library(readr)
 library(gplots)
 
-for (key in c("PGs","17-ABC1Ks")) {
-    for (typ in c("Scaled","Zscore")) {
-        cat(paste0("\n","-- ",key,",",typ," --","\n"))
-        data_file = paste0(key,'_',typ,'_data.csv')
-        dendro_file = paste0(key,'_',typ,'_clusters.png')
-
-        DATA = readr::read_csv(data_file)
-        dim(DATA)
-        colnames(DATA)
-        D = DATA[,-1]
-        # sapply(D,class)
-        unique(sapply(D,class))
-        # print(head(D))
-
-        # ~~ Dendrogram ~~
-        D_dd = as.dist((1-cor(t(D)))/2)
-        # print(D_dd)
-        D_hc = hclust(D_dd, method="average")
-        png(filename = dendro_file, width=960, height=480, units="px")
-        plot(D_hc, labels=unlist(DATA[,1],use.names=FALSE), hang=-1, frame.plot=FALSE, main=paste0("Heirarchical Clusters: ",key," (using ",typ," abundance)"), sub="", xlab="", ylab="correlation-based distance")
-        # num_clust = 4
-        # rect.hclust(D_hc, k=num_clust, border = 1 + 1:num_clust)
-        dev.off()
-    }
-}
+# for (key in c("PGs","17-ABC1Ks")) {
+#     for (typ in c("Scaled","Zscore")) {
+#         cat(paste0("\n","-- ",key,",",typ," --","\n"))
+#         data_file = paste0(key,'_',typ,'_data.csv')
+#         dendro_file = paste0(key,'_',typ,'_clusters.png')
+# 
+#         DATA = readr::read_csv(data_file)
+#         dim(DATA)
+#         colnames(DATA)
+#         D = DATA[,-1]
+#         # sapply(D,class)
+#         unique(sapply(D,class))
+#         # print(head(D))
+# 
+#         # ~~ Dendrogram ~~
+#         D_dd = as.dist((1-cor(t(D)))/2)
+#         # print(D_dd)
+#         D_hc = hclust(D_dd, method="average")
+#         png(filename = dendro_file, width=960, height=480, units="px")
+#         plot(D_hc, labels=unlist(DATA[,1],use.names=FALSE), hang=-1, frame.plot=FALSE, main=paste0("Heirarchical Clusters: ",key," (using ",typ," abundance)"), sub="", xlab="", ylab="correlation-based distance")
+#         # num_clust = 4
+#         # rect.hclust(D_hc, k=num_clust, border = 1 + 1:num_clust)
+#         dev.off()
+#     }
+# }
 
 
 library(pvclust)
@@ -41,6 +41,7 @@ data_file = paste0(key,'_',typ,'_data.csv')
 DATA = readr::read_csv(data_file)
 D = t(DATA[,-1])
 colnames(D) = unlist(DATA[,1])
-pv = pvclust(D, method.hclust="average", method.dist="correlation", nboot=20)
+# pv = pvclust(D, method.hclust="average", method.dist="correlation", nboot=20)
+pv = pvclust(D, method.hclust="average", method.dist = function(x) as.dist((1-cor(t(x)))/2) , nboot=20)
 plot(pv, hang = -1, cex = 0.5)
 pvrect(pv)

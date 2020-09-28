100bf4d6a08b64d5ec0c8c0a99fb3c512e23cd9c
Lalit Ponnala <lalit.p@gmail.com>
2020-09-27 23:58:17 -0400

    commit

--- 2020-09-21/clust.R
+++ 2020-09-21/clust.R
@@ -7,7 +7,7 @@
 # https://www.datanovia.com/en/blog/clustering-using-correlation-as-distance-measures-in-r/
 
 
-# -- with p-values --
+# -- cluster the proteins (show p-values) --
 library(readr)
 library(pvclust)
 set.seed(123)
@@ -42,7 +42,7 @@ for (key in c("PGs","17-ABC1Ks")) {
 }
 
 
-# # -- initial version (no p-values) --
+# # -- cluster the proteins (no p-values) --
 # library(readr)
 # for (key in c("PGs","17-ABC1Ks")) {
 #     for (typ in c("Scaled","Zscore")) {
@@ -70,4 +70,49 @@ for (key in c("PGs","17-ABC1Ks")) {
 #         dev.off()
 #     }
 # }
+
+
+# -- cluster the tissues (no p-values) --
+library(readr)
+for (key in c("PGs","17-ABC1Ks")) {
+    for (typ in c("Scaled","Zscore")) {
+        cat(paste0("\n","-- ",key,",",typ," --","\n"))
+        data_file = paste0(key,'_',typ,'_data.csv')
+        dendro_file = paste0(key,'_',typ,'_tissue_clusters.png')
+
+        DATA = readr::read_csv(data_file)
+        dim(DATA)
+        colnames(DATA)
+        D = DATA[,-1]
+        # sapply(D,class)
+        unique(sapply(D,class))
+        # print(head(D))
+
+        # ~~ Dendrogram ~~
+        D_dd = as.dist((1-cor(t(D)))/2) # divide by 2 so that the max dist is 1
+        # D_dd = as.dist(1-cor(t(D))) # leave out the "dividing by 2" part
+        # print(D_dd)
+        D_hc = hclust(D_dd, method="average")
+        png(filename = dendro_file, width=960, height=480, units="px")
+        plot(D_hc, labels=unlist(DATA[,1],use.names=FALSE), hang=-1, frame.plot=FALSE, main=paste0("Heirarchical Clusters: ",key," (using ",typ," abundance)"), sub="", xlab="", ylab="correlation-based distance")
+        # num_clust = 4
+        # rect.hclust(D_hc, k=num_clust, border = 1 + 1:num_clust)
+        dev.off()
+    }
+}
+
+
+# # -- heatmap --
+# library(gplots)
+# key = 'PGs'
+# typ = 'Scaled'
+# data_file = paste0(key,'_',typ,'_data.csv')
+# DATA = readr::read_csv(data_file)
+# D = DATA[,-1]
+# hclust_prot = hclust(as.dist((1-cor(t(D)))/2), method="average")
+# hclust_tiss = hclust(as.dist((1-cor(D))/2), method="average")
+# heatmap.2(as.matrix(D), scale = "row", col = bluered(100), 
+#           trace = "none", density.info = "none",
+#           Colv = as.dendrogram(hclust_tiss),
+#           Rowv = as.dendrogram(hclust_prot))
 

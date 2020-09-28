0aab207112ecd466257e6b48a2287d9af90860d0
Lalit Ponnala <lalit.p@gmail.com>
2020-09-27 21:30:25 -0400

    commit

--- 2020-09-21/clust.R
+++ 2020-09-21/clust.R
@@ -3,46 +3,22 @@
 # // Heirarchical Clustering //
 # see script.py for preparing the data
 
-# library(pvclust)
-# set.seed(123)
-# key = 'PGs'
-# typ = 'Scaled'
-# data_file = paste0(key,'_',typ,'_data.csv')
-# DATA = readr::read_csv(data_file)
-# D = t(DATA[,-1])
-# colnames(D) = unlist(DATA[,1])
-# pv = pvclust(D, method.hclust="average", method.dist="correlation", nboot=20)
-# # pv = pvclust(D, method.hclust="average", method.dist = function(x) as.dist((1-cor(t(x)))/2) , nboot=20)
-# plot(pv, hang = -1, cex = 0.5)
-# pvrect(pv)
-# 
-# D = DATA[,-1]
-# D_dd = as.dist(1-cor(t(D)))
-# # print(D_dd)
-# D_hc = hclust(D_dd, method="average")
-# plot(D_hc, labels=unlist(DATA[,1],use.names=FALSE), hang=-1, frame.plot=FALSE, main=paste0("Heirarchical Clusters: ",key," (using ",typ," abundance)"), sub="", xlab="", ylab="correlation-based distance")
-
-# ----------
-
 # heatmap: check out pheatmap()
 # https://www.datanovia.com/en/blog/clustering-using-correlation-as-distance-measures-in-r/
 
-# ----------
 
 # -- with p-values --
 library(readr)
 library(pvclust)
 set.seed(123)
+save_plots = TRUE
 for (key in c("PGs","17-ABC1Ks")) {
     for (typ in c("Scaled","Zscore")) {
         cat(paste0("\n","-- ",key,",",typ," --","\n"))
-        data_file = paste0(key,'_',typ,'_data.csv')
-        dendro_file = paste0(key,'_',typ,'_clusters.png')
 
         # ~~ Data ~~
+        data_file = paste0(key,'_',typ,'_data.csv')
         DATA = readr::read_csv(data_file)
-        dim(DATA)
-        colnames(DATA)
         D = t(DATA[,-1])
         colnames(D) = unlist(DATA[,1])
         unique(sapply(D,class))
@@ -52,10 +28,15 @@ for (key in c("PGs","17-ABC1Ks")) {
         # pv = pvclust(D, method.hclust="average", method.dist = function(x) as.dist((1-cor(t(x)))/2) , nboot=20)
 
         # ~~ Plot ~~
-        # png(filename = dendro_file, width=960, height=480, units="px")
+        if (save_plots) {
+            dendro_file = paste0(key,'_',typ,'_clusters_pval.png')
+            png(filename = dendro_file, width=960, height=480, units="px")
+        }
         plot(pv, hang = -1, cex = 0.5)
         pvrect(pv)
-        # dev.off()
+        if (save_plots) {
+            dev.off()
+        }
     }
 }
 
@@ -77,7 +58,8 @@ for (key in c("PGs","17-ABC1Ks")) {
 #         # print(head(D))
 # 
 #         # ~~ Dendrogram ~~
-#         D_dd = as.dist((1-cor(t(D)))/2)
+#         D_dd = as.dist((1-cor(t(D)))/2) # divide by 2 so that the max dist is 1
+#         # D_dd = as.dist(1-cor(t(D))) # leave out the "dividing by 2" part
 #         # print(D_dd)
 #         D_hc = hclust(D_dd, method="average")
 #         png(filename = dendro_file, width=960, height=480, units="px")

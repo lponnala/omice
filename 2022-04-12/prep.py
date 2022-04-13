87fcb475af2bc1bd7343b4ff1977cc9c28f3baf2
Lalit Ponnala <lalit.p@gmail.com>
2022-04-13 00:35:29 -0400

    commit

--- 2022-04-12/prep.py
+++ 2022-04-12/prep.py
@@ -2,12 +2,11 @@
 import pandas as pd
 from scipy.stats import ttest_ind
 
-X = pd.read_excel("/home/lponnala/repos/omics/2018-02-17/fort-test-Antonset.xlsx")
-X = X.iloc[:28,1:9]
-X = X.rename(columns={'simple functional name (July 2017)':'function'})
+# X = pd.read_excel("/home/lponnala/repos/omics/2018-02-17/fort-test-Antonset.xlsx")
+# X = X.iloc[:28,1:9]
+# X = X.rename(columns={'simple functional name (July 2017)':'function'})
 
-res = ttest_ind(X.iloc[0,2:5], X.iloc[0,5:8], equal_var=False, alternative='two-sided')
-res.statistic, res.pvalue
+X = pd.read_excel("/home/lponnala/repos/omics/")
 
 S = []
 for _,x in X.iterrows():

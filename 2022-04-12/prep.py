f8ddfcc95ccd8b30b12ee5d6b924b16655cff212
Lalit Ponnala <lalit.p@gmail.com>
2022-04-13 13:22:37 -0400

    commit

--- 2022-04-12/prep.py
+++ 2022-04-12/prep.py
@@ -1,29 +1,36 @@
 
 import pandas as pd
 from scipy.stats import ttest_ind
-from statsmodels.stats import multicomp, multitest
+from statsmodels.stats import weightstats, multitest, multicomp
 
 D = pd.read_excel("/home/lponnala/repos/omics/2022-04-12/forlalit-chloroplast-wt-prep1prep2.xlsx")
-X, ofile = D.iloc[:,[0,4,5,6,7,8,9]], 'all.xlsx'
-X, ofile = D[(D.iloc[:,[10,11,12]].sum(axis=1) > 6) | (D.iloc[:,[13,14,15]].sum(axis=1) > 6)].iloc[:,[0,4,5,6,7,8,9]], 'subset.xlsx'
+ttest_from = ['scipy','statsmodels'][0]
+dataset = ['all','subset'][0]
+
+if dataset == 'all':
+    X = D.iloc[:,[0,4,5,6,7,8,9]]
+elif dataset == 'subset':
+    X = D[(D.iloc[:,[10,11,12]].sum(axis=1) > 6) | (D.iloc[:,[13,14,15]].sum(axis=1) > 6)].iloc[:,[0,4,5,6,7,8,9]]
 
 X.shape
 X.dtypes
 
 S = []
 for _,x in X.iterrows():
-    res = ttest_ind(x.iloc[1:4], x.iloc[4:7], equal_var=False, alternative='two-sided')
+    if ttest_from == 'scipy':
+        res = ttest_ind(x.iloc[1:4], x.iloc[4:7], equal_var=False, alternative='two-sided')
+    elif ttest_from == 'statsmodels':
+        res = weightstats.ttest_ind(x.iloc[1:4], x.iloc[4:7], usevar='unequal', alternative='two-sided')
     S.append((x['protein'], res.statistic, res.pvalue))
 
 S = pd.DataFrame(S, columns=['protein','t-stat','p-value'])
-S['infer'] = (S['p-value'] < 0.05).map({True: 'NOTEQ', False: 'EQUAL'})
-S['infer'].value_counts()
-print(S)
+S['inference'] = (S['p-value'] < 0.05).map({True: 'NOTEQ', False: 'EQUAL'})
+S['inference'].value_counts()
+# print(S)
 
 y = multitest.multipletests(S['p-value'], alpha=0.1, method='bonferroni')
-y[1]
-
-multicomp.tukeyhsd()
+S['corrected-p-value'] = y[1]
+# multicomp.tukeyhsd()
 
 # # -- verify previous result --
 # # X = pd.read_excel("/home/lponnala/repos/omics/2018-02-17/fort-test-Antonset.xlsx")

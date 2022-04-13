a90bc77e5283e690fcc6bf59e1ee9338f7b4aed5
Lalit Ponnala <lalit.p@gmail.com>
2022-04-13 13:14:20 -0400

    commit

--- 2022-04-12/prep.py
+++ 2022-04-12/prep.py
@@ -1,6 +1,7 @@
 
 import pandas as pd
 from scipy.stats import ttest_ind
+from statsmodels.stats import multicomp, multitest
 
 D = pd.read_excel("/home/lponnala/repos/omics/2022-04-12/forlalit-chloroplast-wt-prep1prep2.xlsx")
 X, ofile = D.iloc[:,[0,4,5,6,7,8,9]], 'all.xlsx'
@@ -19,6 +20,11 @@ S['infer'] = (S['p-value'] < 0.05).map({True: 'NOTEQ', False: 'EQUAL'})
 S['infer'].value_counts()
 print(S)
 
+y = multitest.multipletests(S['p-value'], alpha=0.1, method='bonferroni')
+y[1]
+
+multicomp.tukeyhsd()
+
 # # -- verify previous result --
 # # X = pd.read_excel("/home/lponnala/repos/omics/2018-02-17/fort-test-Antonset.xlsx")
 # # X = X.iloc[:28,1:9]
@@ -30,7 +36,3 @@ print(S)
 # S = pd.DataFrame(S, columns=['function','location','t-stat','p-value'])
 # S['infer'] = (S['p-value'] < 0.05).map({True: 'NOTEQ', False: 'EQUAL'})
 # print(S)
-
-from statsmodels.stats.multitest import multipletests
-y = multipletests(S['p-value'], alpha=0.1, method='bonferroni')
-y[1]

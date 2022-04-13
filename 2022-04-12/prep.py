2c3b72dcd1ac854bb56902dca5ed6ab7e5e9298a
Lalit Ponnala <lalit.p@gmail.com>
2022-04-13 00:51:15 -0400

    commit

--- 2022-04-12/prep.py
+++ 2022-04-12/prep.py
@@ -2,17 +2,31 @@
 import pandas as pd
 from scipy.stats import ttest_ind
 
-# X = pd.read_excel("/home/lponnala/repos/omics/2018-02-17/fort-test-Antonset.xlsx")
-# X = X.iloc[:28,1:9]
-# X = X.rename(columns={'simple functional name (July 2017)':'function'})
+D = pd.read_excel("/home/lponnala/repos/omics/2022-04-12/forlalit-chloroplast-wt-prep1prep2.xlsx")
+X, ofile = D.iloc[:,[0,4,5,6,7,8,9]], 'all.xlsx'
+X, ofile = D[(D.iloc[:,[10,11,12]].sum(axis=1) > 6) | (D.iloc[:,[13,14,15]].sum(axis=1) > 6)].iloc[:,[0,4,5,6,7,8,9]], 'subset.xlsx'
 
-X = pd.read_excel("/home/lponnala/repos/omics/")
+X.shape
+X.dtypes
 
 S = []
 for _,x in X.iterrows():
-    res = ttest_ind(x.iloc[2:5], x.iloc[5:8], equal_var=False, alternative='two-sided')
-    S.append((x['function'], x['location'], res.statistic, res.pvalue))
+    res = ttest_ind(x.iloc[1:4], x.iloc[4:7], equal_var=False, alternative='two-sided')
+    S.append((x['protein'], res.statistic, res.pvalue))
 
-S = pd.DataFrame(S, columns=['function','location','t-stat','p-value'])
+S = pd.DataFrame(S, columns=['protein','t-stat','p-value'])
 S['infer'] = (S['p-value'] < 0.05).map({True: 'NOTEQ', False: 'EQUAL'})
+S['infer'].value_counts()
 print(S)
+
+# # -- verify previous result --
+# # X = pd.read_excel("/home/lponnala/repos/omics/2018-02-17/fort-test-Antonset.xlsx")
+# # X = X.iloc[:28,1:9]
+# # X = X.rename(columns={'simple functional name (July 2017)':'function'})
+# S = []
+# for _,x in X.iterrows():
+#     res = ttest_ind(x.iloc[2:5], x.iloc[5:8], equal_var=False, alternative='two-sided')
+#     S.append((x['function'], x['location'], res.statistic, res.pvalue))
+# S = pd.DataFrame(S, columns=['function','location','t-stat','p-value'])
+# S['infer'] = (S['p-value'] < 0.05).map({True: 'NOTEQ', False: 'EQUAL'})
+# print(S)
